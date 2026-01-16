9af689dfa47d23c0992f0e86c507e38910bdb22e
braiton1911 <brianbeltran.estudios@gmail.com>
2026-01-16 00:36:36 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "crono-acuatica-v1";
+const CACHE = "crono-acuatica-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
@@ -8,3 +8,4 @@ self.addEventListener("install", (e) => {
 self.addEventListener("fetch", (e) => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
+
