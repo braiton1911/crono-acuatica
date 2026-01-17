6ec157f53d38d050e6922b39e330c9d48c132275
braiton1911 <brianbeltran.estudios@gmail.com>
2026-01-17 02:05:29 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,27 @@
-const CACHE = "crono-acuatica-v9";
+const CACHE = "crono-acuatica-v10";
 
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./assets/logo-crono-acuatica",
+  "./assets/logo-crono-acuatica.png"
+];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : Promise.resolve())))
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
     )
   );
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
-
-
